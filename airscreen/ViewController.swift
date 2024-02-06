4f2ae54b8666e70c2fbed3ca55d675e31073f135
6cessfuldev <6cessfuldev@gmail.com>
2024-02-06 21:20:38 +0900

    Initial Commit

---
+++ airscreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  airscreen
+//
+//  Created by 육성민 on 2/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
