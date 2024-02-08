0be512f944e3efefa18f9a03fe277235eb445fb9
6cessfuldev <6cessfuldev@gmail.com>
2024-02-09 00:54:00 +0900

    add tabbarcontroller

---
+++ airscreen/MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  airscreen
+//
+//  Created by 육성민 on 2/9/24.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("app start")
+    }
+}
