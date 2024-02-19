764c22a35d1740d24834964b53123441ab1777db
6cessfuldev <6cessfuldev@gmail.com>
2024-02-20 00:03:57 +0900

    add CustomNavigationBarView

---
+++ airscreen/views/CustomNavigationBarView.swift
@@ -0,0 +1,22 @@
+//
+//  CustomNavigationBarView.swift
+//  airscreen
+//
+//  Created by 육성민 on 2/17/24.
+//
+
+import UIKit
+
+class CustomNavigationBarView: UINavigationBar {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        self.barTintColor = UIColor.blue
+        self.titleTextAttributes = [
+                    NSAttributedString.Key.foregroundColor: UIColor.white
+                ]
+    }
+}
