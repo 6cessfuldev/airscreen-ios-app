ff7a9c3ce77216777438d2f3d609676e21ef3389
6cessfuldev <6cessfuldev@gmail.com>
2024-03-10 00:28:43 +0900

    팝업버튼 추가

--- airscreen/controllers/FlightListViewController.swift
+++ airscreen/controllers/FlightListViewController.swift
@@ -12,14 +12,62 @@ import Foundation
 class FlightListViewController : UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var terminalPopUpBtn: UIButton!
+    @IBOutlet weak var counterPopUpBtn: UIButton!
     
     var responseData: DepartingFlightsList?
+    
+    let terminals: [String] = ["제1터미널", "제2터미널"]
+    let counters: [String] = [
+        "A",
+        "B1",
+        "B2",
+        "C1",
+        "C2",
+        "D1",
+        "D2",
+        "E1",
+        "E2",
+        "F1",
+        "F2",
+        "G",
+        "H1",
+        "H2",
+        "J1",
+        "J2",
+        "K1",
+        "K2",
+        "L1",
+        "L2",
+        "M1",
+        "M2",
+        "N"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         print("FlightListViewController - viewDidLoad")
+        
+        terminalPopUpBtn.menu = UIMenu(title: "터미널", children: terminals.map { item in
+            UIAction(title: item) { (action) in
+                print("Terminal : \(item)")
+            }
+        })
+        
+        counterPopUpBtn.menu =  UIMenu(title: "카운터", children:  counters.map { item in
+            UIAction(title: item) { (action) in
+                print("Counter : \(item)")
+            }
+        })
+
+        
         getTest()
+    }
+    
+    func function(selectedTerminal: String) {
+      // Implement your logic here based on the selected terminal
+      print("Selected terminal: \(selectedTerminal)")
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
